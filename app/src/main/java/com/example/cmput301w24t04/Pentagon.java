389a8ab3e3f0c46bd40a161dbff973c3791985b2
Aditya Shahi <ashahi1@ualberta.com>
2024-02-02 01:02:56 -0700

    added traingle class

---
+++ app/src/main/java/com/example/cmput301w24t04/Pentagon.java
@@ -0,0 +1,12 @@
+package com.example.cmput301w24t04;
+
+public class pentagon extends Shape{
+    public String color(){
+        color= "green";
+        return color;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/cmput301w24t04/Triangle.java
@@ -0,0 +1,5 @@
+package com.example.cmput301w24t04;
+
+public class Triangle extends Shape{
+
+}
